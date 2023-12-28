a7ddce86e54d6415cf9e6b680f1b2a569e923a48
Deepthisambangi <148567653+Deepthisambangi@users.noreply.github.com>
2023-12-28 12:02:19 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,9 +1,5 @@
-import java.io.*;
-
-class Hello
-{
-public static void main (String[] args)
-{
-System.out.println("Hello World");
-}
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
